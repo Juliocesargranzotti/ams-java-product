737b9351096efd1f139652d51f63b421afb9d60a
Juliocesargranzotti1 <jcgranzotti@gmail.com>
2023-08-18 15:03:10 -0300

    Ajustando erro 500

--- src/main/java/com/example/productbackend/services/ProductService.java
+++ src/main/java/com/example/productbackend/services/ProductService.java
@@ -28,5 +28,8 @@ public class ProductService {
                                 () -> new EntityNotFoundException("Product not found")
                                 );
     }
+
+    public void deleteProductByid(long id) {
+    }
     
 }
