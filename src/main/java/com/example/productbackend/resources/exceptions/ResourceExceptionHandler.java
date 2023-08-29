8bb99344d15de9166a3537742ab18572ee71d4c8
Juliocesargranzotti <jcgranzotti@gmail.com>
2023-08-29 15:48:21 -0300

    Validation

--- src/main/java/com/example/productbackend/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/productbackend/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -14,11 +15,10 @@ import jakarta.servlet.http.HttpServletRequest;
 public class ResourceExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFoundEXception(EntityNotFoundException e,
 
-    public ResponseEntity<StandardError> entityNotFoundEXception(
-                                        EntityNotFoundException e,
-                                         HttpServletRequest request) 
-{
+                                                                 HttpServletRequest request) {
+    
         StandardError error = new StandardError();
         HttpStatus status = HttpStatus.NOT_FOUND;
 
@@ -29,7 +29,29 @@ public class ResourceExceptionHandler {
         error.setPath(request.getRequestURI());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
-
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+
+    public ResponseEntity<ValidationErrors> validationErrorsException(
+                                                                MethodArgumentNotValidException e,
+
+                                                                 HttpServletRequest request) {
     
+        ValidationErrors error = new ValidationErrors();
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+
+        error.setTimeStamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError("Validation Errors");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+
+        e.getBindingResult()
+         .getFieldErrors()
+         .forEach( err -> error.addError(err.getDefaultMessage()));
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    
+    }
 }
